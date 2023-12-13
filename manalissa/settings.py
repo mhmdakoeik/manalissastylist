6f9c2bdd6b72b98a657415de57135c3d2eee3ab4
mhmdakoeik <mhmdkoeik@gmail.com>
2023-12-13 13:08:43 +0200

    modify settings

--- manalissa/settings.py
+++ manalissa/settings.py
@@ -259,7 +259,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'developerworktest@gmail.com'
+EMAIL_HOST_PASSWORD = 'smxr kltr couo vntg'
 
 
 
