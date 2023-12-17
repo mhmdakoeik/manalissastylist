49a04054947b916d8c86e1e47a2e80d0e2e7fa03
unknown <z.dhaini@itchery.net>
2023-12-17 14:41:02 +0300

    Add DataBase postgre configurations

--- manalissa/settings.py
+++ manalissa/settings.py
@@ -90,22 +90,22 @@ WSGI_APPLICATION = 'manalissa.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else :
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'manal_db',
-        'USER': 'hadi',
-        'PASSWORD': 'Hady123123ay',
-        'HOST': 'localhost',
-        'PORT': '',
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else :
+DATABASES = {
+'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'manal_db',
+    'USER': 'hadi',
+    'PASSWORD': 'Hady123123ay',
+    'HOST': 'localhost',
+    'PORT': '',
     }
 }
 
