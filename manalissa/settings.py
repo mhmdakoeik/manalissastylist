4c2bd0bed28da824e2a4497ea58592192fc2969e
unknown <z.dhaini@itchery.net>
2023-12-19 01:05:09 +0300

    remove f

--- manalissa/settings.py
+++ manalissa/settings.py
@@ -292,7 +292,7 @@ EMAIL_HOST_PASSWORD = 'smxr kltr couo vntg'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/staticf/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
 
