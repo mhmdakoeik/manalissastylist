4807336bb222aace4acae4dd6bdef31348a30a0c
hadi <hady_ay@hotmail.com>
2023-12-19 10:59:33 +0000

    Your commit message

--- manalissa/settings.py
+++ manalissa/settings.py
@@ -294,13 +294,15 @@ EMAIL_HOST_PASSWORD = 'smxr kltr couo vntg'
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
+
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/home/staticfiles/'
+MEDIA_ROOT = '/home/staticfiles/img/'
+
 
 
 # Default primary key field type
