7200a112c918e340fa4752ebcb2f9ed46ef217eb
mhmdakoeik <mhmdkoeik@gmail.com>
2023-12-19 15:56:31 +0200

    fix

--- home/views.py
+++ home/views.py
@@ -6,25 +6,21 @@ from django.db.models.signals import pre_delete, pre_save
 
 @receiver([pre_delete, pre_save], sender=Slider)
 def delete_slider_image(sender, instance, **kwargs):
-    if instance.image:
-        instance.image.delete(save=False)
+    instance.image.delete(save=False)
 
 @receiver([pre_delete, pre_save], sender=WhyChooseOurServices)
 def handle_whychoose_icon_files(sender, instance, **kwargs):
     if not instance.pk:
         return  # If it's a new instance, there's nothing to do
-
     try:
         old_instance = WhyChooseOurServices.objects.get(pk=instance.pk)
     except WhyChooseOurServices.DoesNotExist:
         return  # If the instance does not exist, exit the function
-
     # Loop through each icon field and delete the old file if it has been updated
     for i in range(1, 5):
         field_name = f'icon_{i}'
         old_icon = getattr(old_instance, field_name)
         new_icon = getattr(instance, field_name)
-
         if old_icon and old_icon != new_icon:
             old_icon.delete(save=False)  # Delete the old icon file
 
@@ -32,18 +28,15 @@ def handle_whychoose_icon_files(sender, instance, **kwargs):
 def handle_image_files(sender, instance, **kwargs):
     if not instance.pk:
         return  # If it's a new instance, there's nothing to do
-
     try:
         old_instance = Gallery.objects.get(pk=instance.pk)
     except Gallery.DoesNotExist:
         return  # If the instance does not exist, exit the function
-
     # Loop through each icon field and delete the old file if it has been updated
     for i in range(1, 6):
         field_name = f'image_{i}'
         old_image = getattr(old_instance, field_name)
         new_image = getattr(instance, field_name)
-
         if old_image and old_image != new_image:
             old_image.delete(save=False)  # Delete the old icon file
 
