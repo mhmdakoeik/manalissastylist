e6262f80088e6d5d56a484a95cc41fdb15c4fa26
mhmdakoeik <mhmdkoeik@gmail.com>
2024-01-03 13:38:49 +0200

    add newsletter

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -8,4 +8,6 @@ class NewsLetterForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(NewsLetterForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
         self.fields['email'].initial = ''
