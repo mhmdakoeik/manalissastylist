e579a3dbe91765a597c1b8628baaad6e466ccee6
mhmdakoeik <mhmdkoeik@gmail.com>
2024-04-16 13:04:12 +0300

    add changes

--- static/js/gallery-animate.js
+++ static/js/gallery-animate.js
@@ -1,14 +1,22 @@
 const panels = document.querySelectorAll('.panel');
+let currentIndex = 0;
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses();
-        panel.classList.add('active');
-    });
-});
+function toggleActivePanel() {
+    removeActiveClasses();
+    panels[currentIndex].classList.add('active');
+    currentIndex = (currentIndex + 1) % panels.length;
+}
 
 function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove('active');
-    })
+    });
 }
+
+// Set an interval to automatically toggle active panel
+const intervalId = setInterval(toggleActivePanel, 2000); // Change 2000 to the desired interval in milliseconds
+
+// Optionally, you can stop the interval after a certain period
+setTimeout(() => {
+    clearInterval(intervalId);
+}, 10000);
